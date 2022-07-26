3776cc0fbea3591562e2fc61695255ff8cf0f437
simplycodestudio <simplycodestudio@gmail.com>
2022-07-26 09:16:09 +0200

    Initial commit

---
+++ src/main/java/org/lisiecki/hsbcchallenge/twitterprototype/TwitterPrototype.java
@@ -0,0 +1,13 @@
+package org.lisiecki.hsbcchallenge.twitterprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterPrototype {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterPrototype.class, args);
+    }
+
+}
---
+++ src/test/java/org/lisiecki/hsbcchallenge/twitterprototype/TwitterPrototypeTests.java
@@ -0,0 +1,13 @@
+package org.lisiecki.hsbcchallenge.twitterprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterPrototypeTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
